7dc491de04c67c3d957b7bb1d656cb6d93a19695
Crystal Korth <crystalkorth@gmail.com>
2022-08-19 21:50:13 -0500

    added input form, working well

--- src/App.js
+++ src/App.js
@@ -2,21 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState } from 'react'
 import ColorBlock from './ColorBlock'
+import ColorForm from './ColorForm'
 
 function App() {
-  let colors = [
+  let [colors, setColors] = useState( [
     'violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'
-  ]
+  ])
 let colorMap = colors.map((color, i) => {
   return (
     <ColorBlock color = {color} />
   )
 })
 
+const addColor = (newColor) => {
+  setColors([...colors, newColor])
+}
   return (
     <div className="App">
     {colorMap}
-    </div>
+    <ColorForm addColor={addColor} />
+    
+</div>
+
   );
 }
 
